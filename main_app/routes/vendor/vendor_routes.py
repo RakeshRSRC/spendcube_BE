98c5f4694b7b70eedb10df5b49706fd8e636863f
gauniyal rakesh <gauniyal.rakesh@bcg.com>
2019-12-12 15:53:36 +0530

    RSRC: Added API's for fetching client's: 1) Exporting Vendor's Category Region template (Using a dummy file from project location)

--- main_app/routes/vendor/vendor_routes.py
+++ main_app/routes/vendor/vendor_routes.py
@@ -1,9 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, send_file
 
 vendor_mod = Blueprint('vendor', __name__)
 
 
 @vendor_mod.route('/', methods=['GET'])
 def vendor():
-    print("YASSS")
     return "HELLO VENDOR"
+
+
+@vendor_mod.route('/exportRFT', methods=['GET'])
+def return_files_tut():
+    try:
+        print("FILE -->")
+        local_file_path = 'static/dummy_execl.xlsx'
+        return send_file(local_file_path, as_attachment=True)
+    except Exception as e:
+        return str(e)
